bb867c27609c8cd98b7dbe9f09e1b4055c79c5d1
Widi Arrohman <widiarrohman1234@gmail.com>
2023-11-26 22:43:14 +0700

    Update build_push_image_karsajobs.sh

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,19 +1,21 @@
 # build image dengan nama karsajobs:latest
-docker build -t karsajobs:latest . || { echo 'docker build failed' ; exit 1; }
+docker build -t karsajobs:latest . 
 
 # login ke docker hub
-docker login || { echo 'docker login failed' ; exit 1; }
+docker login 
 
 # tag image dengan nama widiarrohman/karsajobs:latest
-docker tag karsajobs:latest widiarrohman/karsajobs:latest || { echo 'docker tag failed' ; exit 1; }
+docker tag karsajobs:latest widiarrohman/karsajobs:latest
 
 # push image ke docker hub
-docker push widiarrohman/karsajobs:latest || { echo 'docker push failed' ; exit 1; }
+docker push widiarrohman/karsajobs:latest
 
 # cara menjalankan file build_push_image_karsajobs.sh
 # chmod +x build_push_image_karsajobs.sh
 # ./build_push_image_karsajobs.sh
 
+# cara membuat container dari image ini
+# docker run -d -p 8080:8080 widiarrohman/karsajobs:latest
 
 
 
