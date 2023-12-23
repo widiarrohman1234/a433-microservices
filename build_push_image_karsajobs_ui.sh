e79285057427ace134cf2a53aa3dc240a5e3b31d
Widi Arrohman <widiarrohman1234@gmail.com>
2023-12-24 05:33:57 +0700

    UDPATE: .sh .env catatan.txt

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,14 +1,28 @@
-# build image dengan nama karsajobs-ui:latest
-docker build -t karsajobs-ui:latest . || { echo 'docker build failed' ; exit 1; }
+#!/bin/bash
 
-# login ke docker hub
-docker login || { echo 'docker login failed' ; exit 1; }
+# Nama Docker image untuk frontend
+IMAGE_NAME="widiarrohman/karsajobs-ui:latest"
+
+# Build Docker image dari Dockerfile
+docker build -t $IMAGE_NAME .
+
+# Login ke Docker Hub
+# docker login -u <USERNAME> -p <PASSWORD>
+
+# jalankan dulu di terminal
+# export PASSWORD_DOCKER_HUB=<password_Anda>
+
+# Login ke Docker Hub dengan menggunakan environment variable
+echo $PASSWORD_DOCKER_HUB | docker login -u widiarrohman --password-stdin
 
 # tag image dengan nama widiarrohman/karsajobs-ui:latest
-docker tag karsajobs-ui:latest widiarrohman/karsajobs-ui:latest || { echo 'docker tag failed' ; exit 1; }
+docker tag karsajobs-ui:latest widiarrohman/karsajobs-ui:latest
 
-# push image ke docker hub
-docker push widiarrohman/karsajobs-ui:latest || { echo 'docker push failed' ; exit 1; }
+# Push image ke Docker Hub
+docker push $IMAGE_NAME
+
+# Logout dari Docker Hub
+docker logout
 
 # cara menjalankan file build_push_image_karsajobs_ui.sh
 # chmod +x build_push_image_karsajobs_ui.sh
@@ -16,7 +30,5 @@ docker push widiarrohman/karsajobs-ui:latest || { echo 'docker push failed' ; ex
 
 # cara menjalankan image karsajobs-ui:latest
 # docker run -d -p 8000:8000 widiarrohman/karsajobs-ui:latest
-
-
 
 
