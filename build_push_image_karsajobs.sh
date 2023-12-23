9e06e232f9e05f00adea3fd8aea84bcc00424241
Widi Arrohman <widiarrohman1234@gmail.com>
2023-12-24 05:53:13 +0700

    Update build_push_image_karsajobs.sh

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,14 +1,28 @@
-# build image dengan nama karsajobs:latest
-docker build -t karsajobs:latest . 
+#!/bin/bash
 
-# login ke docker hub
-docker login 
+# Nama Docker image untuk backend
+IMAGE_NAME="widiarrohman/karsajobs:latest"
+
+# Build Docker image dari Dockerfile
+docker build -t $IMAGE_NAME .
+
+# Login ke Docker Hub
+# docker login -u <USERNAME> -p <PASSWORD>
+
+# jalankan dulu di terminal
+# export PASSWORD_DOCKER_HUB=<password_Anda>
+
+# Login ke Docker Hub dengan menggunakan environment variable
+echo $PASSWORD_DOCKER_HUB | docker login -u widiarrohman --password-stdin
 
 # tag image dengan nama widiarrohman/karsajobs:latest
 docker tag karsajobs:latest widiarrohman/karsajobs:latest
 
-# push image ke docker hub
-docker push widiarrohman/karsajobs:latest
+# Push image ke Docker Hub
+docker push $IMAGE_NAME
+
+# Logout dari Docker Hub
+docker logout
 
 # cara menjalankan file build_push_image_karsajobs.sh
 # chmod +x build_push_image_karsajobs.sh
